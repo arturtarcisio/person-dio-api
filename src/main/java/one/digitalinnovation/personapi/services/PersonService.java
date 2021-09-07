4c73fdb039c068f72e6786e65a1722e3ddc77785
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-09-07 11:59:04 -0300

    Adicionando dependencias do bean e hibernate validator e criando o padrao DTO para nossas entidades

--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi.services;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
@@ -17,8 +18,11 @@ public class PersonService {
 //        this.personRepository = personRepository;
 //    }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+
+        Person personToSave = Person.builder().build();
+
+        Person savedPerson = personRepository.save(personDTO);
         return MessageResponseDTO
                 .builder()
                 .message("Person created with id: " + savedPerson.getId())
